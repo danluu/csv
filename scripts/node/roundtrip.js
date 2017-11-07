04d23dfa0dfe92dd38347e1c6775716ae4c240a1
Wesley Aptekar-Cassels <W.Aptekar@gmail.com>
2017-11-06 20:34:00 -0500

    Make node test script write to file

--- scripts/node/roundtrip.js
+++ scripts/node/roundtrip.js
@@ -6,7 +6,11 @@ fs.readFile(process.argv[2], {encoding: 'utf-8'}, function(err,data){
 	if (!err) {
 		csv.parse(data, function(err, data) {
 			csv.stringify(data, function(err, data) {
-				console.log(data);
+				fs.writeFile(process.argv[3], data, function(err) {
+					if (err) {
+						console.log(err);
+					}
+				});
 			});
 		});
 	} else {
